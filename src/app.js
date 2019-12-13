041a0a1dde9378d0314468bdc5e2c20214428a71
Godopu <godopu16@gmail.com>
2019-12-13 18:33:45 +0900

    update putStatus

--- src/app.js
+++ src/app.js
@@ -9,12 +9,47 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const http = __importStar(require("http"));
 const config = require("../config.json");
+let parameters = ["open", "close"];
+let idx = 0;
+function putStatus() {
+    let obj = {
+        state: parameters[idx]
+    };
+    let options = {
+        hostname: config["ip-adr"],
+        port: config["port"],
+        path: "/door",
+        method: "put",
+        headers: {
+            "Content-Type": "application/json",
+            "Content-Length": JSON.stringify(obj).length
+        }
+    };
+    idx = 1 - idx;
+    return new Promise(resolve => {
+        let req = http.request(options, function (res) {
+            res.setEncoding("utf8");
+            let retValue = "";
+            res.on("data", (body) => {
+                retValue += body;
+            });
+            res.on("error", function (e) {
+                console.log("Problem with request: " + e.message);
+            });
+            res.on("end", () => {
+                resolve(retValue);
+            });
+        });
+        req.write(JSON.stringify(obj));
+        req.end();
+    });
+}
 function getStatus() {
     let options = {
         hostname: config["ip-adr"],
         port: config["port"],
         path: "/door-latest",
-        method: "GET",
+        method: "get",
     };
     return new Promise(resolve => {
         let req = http.request(options, function (res) {
@@ -36,11 +71,14 @@ function getStatus() {
 let timer = null;
 let status = "close";
 (async function main() {
+    // setInterval(async () => {
+    //     let retValue = await getStatus()
+    //     if(retValue === status) return;
+    //     status = retValue
+    //     console.log(status);
+    // }, 1000);
     setInterval(async () => {
-        let retValue = await getStatus();
-        if (retValue === status)
-            return;
-        status = retValue;
-        console.log(status);
-    }, 1000);
+        let retValue = await putStatus();
+        console.log(retValue);
+    }, 3000);
 })();
--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,57 @@ import * as http from "http";
 
 const config = require("../config.json");
 
+let parameters = ["open", "close"];
+let idx = 0;
+
+function putStatus() : Promise<string>{
+
+    let obj = {
+        state : parameters[idx]
+    }
+
+    let options : http.RequestOptions = {
+        hostname : config["ip-adr"],
+        port : config["port"],
+        path : "/door",
+        method : "put",
+        headers : {
+            "Content-Type" : "application/json",
+            "Content-Length" : JSON.stringify(obj).length
+        }
+    }; 
+    idx = 1-idx;
+
+
+    return new Promise<string>(resolve => {
+        
+        let req = http.request(options, function(res){
+            res.setEncoding("utf8");
+            
+            let retValue = ""
+            res.on("data", (body)=>{
+                retValue += body
+            })
+            res.on("error", function(e){
+                console.log("Problem with request: " + e.message);
+            });
+            res.on("end", ()=>{
+                resolve(retValue)
+            })
+            
+        });
+        req.write(JSON.stringify(obj));
+        req.end();
+    });
+}
+
 function getStatus() : Promise<string>{
 
     let options : http.RequestOptions = {
         hostname : config["ip-adr"],
         port : config["port"],
         path : "/door-latest",
-        method : "GET",
+        method : "get",
     }; 
     
     return new Promise<string>(resolve => {
@@ -38,11 +82,16 @@ let timer : NodeJS.Timeout | null = null;
 let status = "close";
 (async function main()
 {
-    setInterval(async () => {
-        let retValue = await getStatus()
-        if(retValue === status) return;
+    // setInterval(async () => {
+    //     let retValue = await getStatus()
+    //     if(retValue === status) return;
 
-        status = retValue
-        console.log(status);
-    }, 1000);
+    //     status = retValue
+    //     console.log(status);
+    // }, 1000);
+
+    setInterval(async () => {
+        let retValue = await putStatus()
+        console.log(retValue);
+    }, 3000);
 })();
